ab64c82872490996ce43ff6c69e0c6d64b16120a
craig wadding <craig.wadding@gmail.com>
2019-06-09 02:30:01 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song 
+  @@count = 0 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  @@genres = []
+  
+  def self.genres 
+    @@genres.uniq 
+  end 
+  
+  @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total} 
+end 
+
+def self.grenre_count 
+  grenre_count = {} 
+  @@genres.each do |grenre| 
+    if grenre_count[genre]
+      grenre_count[genre] += 1 
+    else 
+      grenre_count[genre] = 1 
+    end 
+  end 
+  grenre_count
+end 
+
+@@artists = []
+
+def self.artist_count
+  @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total} 
+end 
+
+def self.artists 
+  @@artists.uniq 
+end 
+
+def initialize(name, artist, genre) 
+  @name = name 
+  @artist = artist 
+  @@artists << artist 
+  @genre = genre 
+  @@genre << genre 
+  @@count += 1 
+end 
+attr_accessor :name, :artist, :genre 
+end  
+
