df8e545a318da9d4605e55bd58c383580b28a082
craig wadding <craig.wadding@gmail.com>
2019-06-09 02:47:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,57 +1,51 @@
-class Song
-
-  attr_accessor :name, :artist, :genre
-
-  @@song_count = 0
+class Song 
+  @@count = 0 
+  
+  def self.count 
+    @@count 
+  end 
+  
   @@genres = []
-  @@artists = []
+  
+  def self.genres 
+    @@genres.uniq 
+  end 
+  
+  @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total} 
+end 
 
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@genres << @genre
-    @@artists << @artist
-    @@song_count +=1
-  end
+def self.grenre_count 
+  grenre_count = {} 
+  @@genres.each do |grenre| 
+    if grenre_count[genre]
+      grenre_count[genre] += 1 
+    else 
+      grenre_count[genre] = 1 
+    end 
+  end 
+  grenre_count
+end 
 
-  def self.count
-    @@song_count
-  end
+@@artists = []
 
-  def self.genres
-    @@genres.uniq
-  end
+def self.artist_count
+  @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total} 
+end 
 
-  def self.artists
-    @@artists.uniq
-  end
+def self.artists 
+  @@artists.uniq 
+end 
 
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre| 
-      if genre_count[genre]
-      genre_count[genre] += 1 
-      else
-      genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
-
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-      artist_count[artist] +=1
-      else
-      artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-
-end
+def initialize(name, artist, genre) 
+  @name = name 
+  @artist = artist 
+  @@artists << artist 
+  @genre = genre 
+  @@genre << genre 
+  @@count += 1 
+end 
+attr_accessor :name, :artist, :genre 
+end  
 
 ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "Rap")
 mask_off = Song.new("Mask Off", "Future", "Rap")
